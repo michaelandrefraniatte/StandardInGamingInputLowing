60f23d4a3c195019b5ec9a6c8973be26508d0a8d
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-01-01 11:12:32 +0100

    Fix a bug

--- Src/StandardInGamingInputLowing/V3/SIGIL/Device.Net/Logging/LoggerFactoryExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Microsoft.Extensions.Logging.Abstractions
-{
-    public static class LoggerFactoryExtensions
-    {
-        public static ILogger<T> CreateLogger<T>(this ILoggerFactory loggerFactory) => new NullLogger<T>(loggerFactory);
-    }
-}
