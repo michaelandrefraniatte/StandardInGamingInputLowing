a57e87dfacf05075c55cdfb2acc2a71f2361614a
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-09 18:46:51 +0100

    Add about

--- Src/WindowTitleRemover/WindowTitleRemover/Form1.cs
+++ Src/WindowTitleRemover/WindowTitleRemover/Form1.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -65,6 +60,23 @@ namespace WindowTitleRemover
                     wu[n] = wu[n] + 1;
                 }
                 wd[n] = 0;
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        private void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
             }
         }
         private void Form1_Load(object sender, EventArgs e)
