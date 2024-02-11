b5811f769cbbd5cc42a7db02615d0d584a932d6f
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-11 19:46:21 +0100

    Fix a bug

--- Src/StandardInGamingInputLowing/Dependencies/keyboardmouse/keyboardmouse/Source.cpp
+++ Src/StandardInGamingInputLowing/Dependencies/keyboardmouse/keyboardmouse/Source.cpp
@@ -8,8 +8,8 @@
 #pragma comment(lib, "winmm.lib")
 #pragma comment(lib, "ntdll.lib")
 INPUT down[1], up[1], downa[1], upa[1], MiceW3[1], Micek[1], Micel[1], Micelf[1], Micerc[1], Micercf[1], Micemc[1], Micemcf[1], Micewd[1], Micewu[1];
-bool downb, upb, downab, upab, MiceW3b, Micekb, Micelb, Micelfb, Micercb, Micercfb, Micemcb, Micemcfb, Micewdb, Micewub;
 int size = sizeof(INPUT);
+
 extern "C"
 {
 	__declspec(dllexport) void MoveMouseTo(int x, int y)
@@ -22,24 +22,16 @@ extern "C"
 	}
 	__declspec(dllexport) void MouseMW3(int x, int y)
 	{
-		if (!MiceW3b)
-		{
-			MiceW3[0].type = 0;
-			MiceW3[0].mi.dwFlags = 0x8001;
-			MiceW3b = true;
-		}
+		MiceW3[0].type = 0;
+		MiceW3[0].mi.dwFlags = 0x8001;
 		MiceW3[0].mi.dx = x;
 		MiceW3[0].mi.dy = y;
 		SendInput(1, MiceW3, size);
 	}
 	__declspec(dllexport) void MouseBrink(int x, int y)
 	{
-		if (!Micekb)
-		{
-			Micek[0].type = 0;
-			Micek[0].mi.dwFlags = 0x0001;
-			Micekb = true;
-		}
+		Micek[0].type = 0;
+		Micek[0].mi.dwFlags = 0x0001;
 		Micek[0].mi.dx = x;
 		Micek[0].mi.dy = y;
 		SendInput(1, Micek, size);
@@ -96,35 +88,23 @@ extern "C"
 	}
 	__declspec(dllexport) void SimulateKeyDown(UINT keyCode, UINT bScan)
 	{
-		if (!downb)
-		{
-			down[0].type = 1;
-			down[0].ki.dwFlags = 0;
-			downb = true;
-		}
+		down[0].type = 1;
+		down[0].ki.dwFlags = 0;
 		down[0].ki.wVk = keyCode;
 		down[0].ki.wScan = bScan;
 		SendInput(1, down, size);
 	}
 	__declspec(dllexport) void SimulateKeyUp(UINT keyCode, UINT bScan)
 	{
-		if (!upb)
-		{
-			up[0].type = 1;
-			up[0].ki.dwFlags = 0x0002;
-			upb = true;
-		}
+		up[0].type = 1;
+		up[0].ki.dwFlags = 0x0002;
 		up[0].ki.wVk = keyCode;
 		up[0].ki.wScan = bScan;
 		SendInput(1, up, size);
 	}
 	__declspec(dllexport) void SimulateKeyDownArrows(UINT keyCode, UINT bScan)
 	{
-		if (!downab)
-		{
-			downa[0].type = 1;
-			downab = true;
-		}
+		downa[0].type = 1;
 		downa[0].ki.wVk = keyCode;
 		downa[0].ki.wScan = bScan;
 		downa[0].ki.dwFlags = 0x0001 | 0x0008;
@@ -134,11 +114,7 @@ extern "C"
 	}
 	__declspec(dllexport) void SimulateKeyUpArrows(UINT keyCode, UINT bScan)
 	{
-		if (!upab)
-		{
-			upa[0].type = 1;
-			upab = true;
-		}
+		upa[0].type = 1;
 		upa[0].ki.wVk = keyCode;
 		upa[0].ki.wScan = bScan;
 		upa[0].ki.dwFlags = 0x0002 | 0x0001 | 0x0008;
@@ -148,84 +124,52 @@ extern "C"
 	}
 	__declspec(dllexport) void LeftClick()
 	{
-		if (!Micelb)
-		{
-			Micel[0].type = 0;
-			Micel[0].mi.dwFlags = 0x0002;
-			Micelb = true;
-		}
+		Micel[0].type = 0;
+		Micel[0].mi.dwFlags = 0x0002;
 		SendInput(1, Micel, size);
 	}
 	__declspec(dllexport) void LeftClickF()
 	{
-		if (!Micelfb)
-		{
-			Micelf[0].type = 0;
-			Micelf[0].mi.dwFlags = 0x0004;
-			Micelfb = true;
-		}
+		Micelf[0].type = 0;
+		Micelf[0].mi.dwFlags = 0x0004;
 		SendInput(1, Micelf, size);
 	}
 	__declspec(dllexport) void RightClick()
 	{
-		if (!Micercb)
-		{
-			Micerc[0].type = 0;
-			Micerc[0].mi.dwFlags = 0x0008;
-			Micercb = true;
-		}
+		Micerc[0].type = 0;
+		Micerc[0].mi.dwFlags = 0x0008;
 		SendInput(1, Micerc, size);
 	}
 	__declspec(dllexport) void RightClickF()
 	{
-		if (!Micercfb)
-		{
-			Micercf[0].type = 0;
-			Micercf[0].mi.dwFlags = 0x0010;
-			Micercfb = true;
-		}
+		Micercf[0].type = 0;
+		Micercf[0].mi.dwFlags = 0x0010;
 		SendInput(1, Micercf, size);
 	}
 	__declspec(dllexport) void MiddleClick()
 	{
-		if (!Micemcb)
-		{
-			Micemc[0].type = 0;
-			Micemc[0].mi.dwFlags = 0x0020;
-			Micemcb = true;
-		}
+		Micemc[0].type = 0;
+		Micemc[0].mi.dwFlags = 0x0020;
 		SendInput(1, Micemc, size);
 	}
 	__declspec(dllexport) void MiddleClickF()
 	{
-		if (!Micemcfb)
-		{
-			Micemcf[0].type = 0;
-			Micemcf[0].mi.dwFlags = 0x0040;
-			Micemcfb = true;
-		}
+		Micemcf[0].type = 0;
+		Micemcf[0].mi.dwFlags = 0x0040;
 		SendInput(1, Micemcf, size);
 	}
 	__declspec(dllexport) void WheelDownF()
 	{
-		if (!Micewdb)
-		{
-			Micewd[0].type = 0;
-			Micewd[0].mi.mouseData = -120;
-			Micewd[0].mi.dwFlags = 0x0800;
-			Micewdb = true;
-		}
+		Micewd[0].type = 0;
+		Micewd[0].mi.mouseData = -120;
+		Micewd[0].mi.dwFlags = 0x0800;
 		SendInput(1, Micewd, size);
 	}
 	__declspec(dllexport) void WheelUpF()
 	{
-		if (!Micewub)
-		{
-			Micewu[0].type = 0;
-			Micewu[0].mi.mouseData = 120;
-			Micewu[0].mi.dwFlags = 0x0800;
-			Micewub = true;
-		}
+		Micewu[0].type = 0;
+		Micewu[0].mi.mouseData = 120;
+		Micewu[0].mi.dwFlags = 0x0800;
 		SendInput(1, Micewu, size);
 	}
 }
